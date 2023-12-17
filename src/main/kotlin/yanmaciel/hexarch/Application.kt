c636efc2ee48fdb1164acbed850f58420940ad27
YanMaciel <araujo.yan@hotmail.com>
2023-12-17 19:44:54 -0300

    first commit: spring boot structure

---
+++ src/main/kotlin/yanmaciel/hexarch/Application.kt
@@ -0,0 +1,11 @@
+package yanmaciel.hexarch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinHexagonalTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKotlinHexagonalTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/yanmaciel/hexarch/SpringBootKotlinHexagonalTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package yanmaciel.hexarch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinHexagonalTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
